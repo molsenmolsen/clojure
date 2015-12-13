8a3ed2be85cfd0d4419b0f0e8fb630c3925e926e
molsenmolsen <molsenmolsen@hotmail.com>
2015-12-13 21:06:54 +0000

    ange to the readme:x

--- my-stuff/project.clj
+++ my-stuff/project.clj
@@ -1,5 +1,5 @@
 (defproject my-stuff "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "learning"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
